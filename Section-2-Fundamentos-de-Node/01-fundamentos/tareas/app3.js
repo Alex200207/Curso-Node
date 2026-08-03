384236ee28cb95aba093aafcb0cfca82064024aa
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-08-03 16:46:27 -0600

    Add app3.js to count total and 'react' words from README.md

---
+++ Section-2-Fundamentos-de-Node/01-fundamentos/tareas/app3.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const content = fs.readFileSync('../README.md', 'utf-8');
+
+const wordCount = content.split(' ');
+
+console.log('Palabras', wordCount.length);
+
+// Tareas contar la cantidad de palabras react
+
+
+const reactCount2 = wordCount.filter(w => w.toLowerCase() === 'react').length
+
+const reactCount = content.match(/react/gi).length;
+
+console.log('cantidad de palabras react', reactCount);
+console.log('cantidad de palabras react filter', reactCount2);

dc4d4be864e8dc5389ff640273c4374b3cfd0c0c
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-08-05 14:49:49 -0600

    Fix react word count logic and improve console output in app3.js

--- Section-2-Fundamentos-de-Node/01-fundamentos/tareas/app3.js
+++ Section-2-Fundamentos-de-Node/01-fundamentos/tareas/app3.js
@@ -9,9 +9,9 @@ console.log('Palabras', wordCount.length);
 // Tareas contar la cantidad de palabras react
 
 
-const reactCount2 = wordCount.filter(w => w.toLowerCase() === 'react').length
+const reactCount2 = wordCount.filter(w => w.toLowerCase().includes('react')).length
 
 const reactCount = content.match(/react/gi).length;
 
-console.log('cantidad de palabras react', reactCount);
+console.log('cantidad de palabras react con expresion regular', reactCount);
 console.log('cantidad de palabras react filter', reactCount2);

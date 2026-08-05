0ddd53c57bf09a50417424854982cff48541fb6f
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-08-05 16:46:53 -0600

    Comment out template export in app.js and add destructuring example in 02-destructuring.js

--- Section-3-desarrollando-en-node/bases/src/app.js
+++ Section-3-desarrollando-en-node/bases/src/app.js
@@ -1,2 +1,3 @@
-const templateExport = require('./js-foundation/01-template.js');
-console.log(templateExport);
+// const templateExport = require('./js-foundation/01-template.js');
+// console.log(templateExport);
+require('./js-foundation/02-destructuring');

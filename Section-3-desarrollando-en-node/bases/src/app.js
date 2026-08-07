14e7a01fcb50ed077c736d2ad4fbf3ef63daa3bd
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-08-07 12:38:14 -0600

    Implement user retrieval functionality with getUserById in callbacks module

--- Section-3-desarrollando-en-node/bases/src/app.js
+++ Section-3-desarrollando-en-node/bases/src/app.js
@@ -1,3 +1,12 @@
 // const templateExport = require('./js-foundation/01-template.js');
 // console.log(templateExport);
-require('./js-foundation/02-destructuring');
+// require('./js-foundation/02-destructuring');
+const { getUserById } = require('./js-foundation/03-callbacks');
+
+const id = 2;
+getUserById(id, (error, user) => {
+  if (error) {
+    throw new Error('No existe el usuario con id: ' + id);
+  }
+  console.log(user);
+});

ca8e6a927bd0d69a1eb32bff936de6565de117bc
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-08-07 13:04:41 -0600

    Refactor buildPerson function to use uuid for id generation and getAge for age calculation

--- Section-3-desarrollando-en-node/bases/src/js-foundation/05-factory.js
+++ Section-3-desarrollando-en-node/bases/src/js-foundation/05-factory.js
@@ -4,12 +4,15 @@ que crea una funcion
 
 // objetivo: deberiamos ser capaces de tener dependencias en nuestro codigo
 
+const { v4: uuidv4 } = require('uuid');
+const getAge = require('get-age');
+
 const buildPerson = ({ name, birthday }) => {
   return {
-    id: new Date().getTime(),
+    id: v4(),
     name,
     birthday,
-    age: new Date().getFullYear() - new Date(birthday).getFullYear(),
+    age: getAge(birthday),
   };
 };
 

30b92009781e4948df8bfd2caa307b4f0b001ddd
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-08-07 12:39:49 -0600

    Improve error handling in getUserById callback to throw specific error message

--- Section-3-desarrollando-en-node/bases/src/app.js
+++ Section-3-desarrollando-en-node/bases/src/app.js
@@ -6,7 +6,7 @@ const { getUserById } = require('./js-foundation/03-callbacks');
 const id = 2;
 getUserById(id, (error, user) => {
   if (error) {
-    throw new Error('No existe el usuario con id: ' + id);
+    throw new Error(error);
   }
   console.log(user);
 });

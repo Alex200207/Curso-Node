4f8861b75fa0a9dd27c7a5a7c84072586b1e2b59
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-08-09 15:58:49 -0600

    feat: add httpClientPlugin for API requests and refactor getPokemonById function
    - Introduced httpClientPlugin to handle HTTP requests using fetch.
    - Updated getPokemonById to utilize httpClientPlugin for fetching Pokémon data.
    - Added axios dependency for potential future HTTP requests.
    - Updated pnpm workspace state and lock files to reflect changes.

--- Section-3-desarrollando-en-node/bases/src/js-foundation/06-promises.js
+++ Section-3-desarrollando-en-node/bases/src/js-foundation/06-promises.js
@@ -30,13 +30,24 @@
 // };
 
 // async transforma valor de retorno a promesa
+// const getPokemonById = async (id) => {
+//   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+//   // el await es codigo bloqueante pues no permite avanzar hasta resolver
+//   const resp = await fetch(url);
+//   const pokemon = await resp.json();
+
+//   return pokemon.name;
+// };
+
+const {httpClientPlugin} = require('../plugins/index');
+
 const getPokemonById = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-  // el await es codigo bloqueante pues no permite avanzar hasta resolver
-  const resp = await fetch(url);
-  const pokemon = await resp.json();
+  const pokemon = await httpClientPlugin.get( url )
 
   return pokemon.name;
 };
+
 module.exports = getPokemonById;

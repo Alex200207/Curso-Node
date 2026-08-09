923283617350318c6be06fce814bfc1f5a89d06c
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-08-09 16:08:44 -0600

    refactor: replace fetch with axios in httpClientPlugin for improved API handling

--- Section-3-desarrollando-en-node/bases/src/plugins/http-client.plugin.js
+++ Section-3-desarrollando-en-node/bases/src/plugins/http-client.plugin.js
@@ -1,9 +1,22 @@
 // adaptado el fetch con nuestro patron adaptador.
 
+const axios = require('axios');
+
+// const httpClientPlugin = {
+//   get: async (url) => {
+//     const resp = await fetch(url);
+//     return await resp.json();
+//   },
+
+//   post: async (url, body) => {},
+//   put: async (url, body) => {},
+//   delete: async (url, body) => {},
+// };
+
 const httpClientPlugin = {
   get: async (url) => {
-    const resp = await fetch(url);
-    return await resp.json();
+    const { data } = await axios.get(url);
+    return data;
   },
 
   post: async (url, body) => {},

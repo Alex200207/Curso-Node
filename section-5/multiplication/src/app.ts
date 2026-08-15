fdd5996f40778b5f660334e43c35d22339dbf2ea
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-08-15 15:20:19 -0600

    feat: implement multiplication table generation and file output

--- section-5/multiplication/src/app.ts
+++ section-5/multiplication/src/app.ts
@@ -1,2 +1,25 @@
-const msg: string = 'Hola Mundo!';
-console.log(msg);
+import fs from 'fs';
+
+let ouputMessge = '';
+const base = 5;
+const header = `
+=============================
+      Tabla del ${base} 
+=============================
+`;
+
+for (let i = 1; i <= 10; i++) {
+  ouputMessge += `${base} x ${i} = ${base * i}\n `;
+}
+
+ouputMessge = header + ouputMessge;
+
+console.log(ouputMessge);
+
+const ouputPath = `outputs`;
+
+// crear folder y los crea d forma recursiva
+fs.mkdirSync(ouputPath, {recursive: true})
+fs.writeFileSync(`${ouputPath}/tabla-${base}.txt`, ouputMessge);
+
+console.log('file created');

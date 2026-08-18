e8082d0b830b6872f405b9595d438a06ade43e18
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-08-18 14:03:20 -0600

    refactor: comment out yargs import and console log in app.ts

--- section-5/multiplication/src/app.ts
+++ section-5/multiplication/src/app.ts
@@ -1,5 +1,19 @@
 // los argv son argumnets values
 
-import { yarg } from './plugins/yargs.plugin.js';
+// import { yarg } from './plugins/yargs.plugin.js';
 
-console.log(yarg);
+// console.log(yarg);
+
+
+// funcion anonima autoinvocada tan pronto se corra el proceso 
+// se ejecutara automaticamente
+
+(async () => {
+    await main()
+})()
+
+
+
+async function main() {
+    console.log('main ejecutado')
+}

6fb767a501f8cec18873e10701dcb88f6e629248
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-08-18 14:22:27 -0600

    refactor: improve formatting and comments in argument options for clarity

--- section-5/multiplication/src/config/plugins/args.plugin.ts
+++ section-5/multiplication/src/config/plugins/args.plugin.ts
@@ -4,9 +4,21 @@ import { hideBin } from 'yargs/helpers';
 export const yarg = yargs(hideBin(process.argv))
   .option('b', {
     // opcion
-    alias: 'base',// nombre de la bandera
-    type: 'number',// el tipo de la bandera
-    demandOption: true,// obliga a pasar la opcion
-    describe:'Multiplication table base'// descripcion
+    alias: 'base', // nombre de la bandera
+    type: 'number', // el tipo de la bandera
+    demandOption: true, // obliga a pasar la opcion
+    describe: 'Multiplication table base', // descripcion
+  })
+  .option('l', {
+    alias: 'limit',
+    type: 'number',
+    default: 10,
+    describe: 'Multiplication table limit',
+  })
+  .option('s', {
+    alias: 'show',
+    type: 'boolean',
+    default: false,
+    describe: 'Show multiplication table',
   })
   .parseSync();

47fae380fc331e5082736c01a031faf9443c27cc
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2026-08-18 16:49:17 -0600

    feat: refactor ServerApp.run method to destructure options and improve logging

--- section-5/multiplication/src/presentation/server-app.ts
+++ section-5/multiplication/src/presentation/server-app.ts
@@ -1,3 +1,5 @@
+import { CreateTable } from '../../domain/use-cases/create-table.use-case.js';
+
 interface RunOptions {
   base: number;
   limit: number;
@@ -7,8 +9,9 @@ interface RunOptions {
 //orquestador de como funcionara nuestra app
 export class ServerApp {
   // este mtodo no necesita iniciarlizar la clase
-  static run(options: RunOptions) {
+  static run({ base, limit, show }: RunOptions) {
     console.log('Server running...');
-    console.log({options})
+    const table = new CreateTable().execute({ base, limit });
+    if(show) console.log(table)
   }
 }
